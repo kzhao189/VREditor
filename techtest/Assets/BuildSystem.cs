2269a2d4bdd4f98b732e0cbe03460b34577fded7
kzhao189 <kyzhao@usc.edu>
2022-08-26 15:35:34 -0700

    color selection now works

--- techtest/Assets/BuildSystem.cs
+++ techtest/Assets/BuildSystem.cs
@@ -23,32 +23,28 @@ public class BuildSystem : MonoBehaviour
     public float fadeOutTime;
     public float holdToDestroyTime;
 
+    private Color myColor;
+    private GameObject[] tempList;
+
     // Start is called before the first frame update
     void Start()
     {
         previewBlockObject = Instantiate(previewBlockObject, new Vector3(0,0,0), Quaternion.identity);
+        myColor = Color.white;
+        tempList = GameObject.FindGameObjectsWithTag("Toggleable");
+        foreach (GameObject a in tempList)
+            a.GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo))
+        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo)) //our raycast!
         {
-            /*if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //returns the face of the object we hit
-            {
-                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2),
-                    Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
-                previewBlockObject.gameObject.transform.position = spawnPosition;
-                //print(spawnPosition);
-            }
-            else if (hitInfo.transform.tag != "UI")
-            {
-                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
-                previewBlockObject.gameObject.transform.position = spawnPosition;
-            }*/
             Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
             if (hitInfo.transform.tag != "UI")
             {
+                //sets previewBlock position (transparent material)
                 previewBlockObject.GetComponent<Renderer>().material = transparentMat;
                 previewBlockObject.gameObject.transform.position = spawnPosition;
                 if (hitInfo.transform.tag == "Block") //switch the previewBlock mat if space is occupied
@@ -59,60 +55,66 @@ public class BuildSystem : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(0)) //left mouse button
         {
-            BuildBlock(blockObject);
+            if (hitInfo.transform.tag != "UI")
+            {
+                BuildBlock(blockObject, hitInfo);
+            } 
+            else if (hitInfo.transform.tag == "UI")
+            {
+                myColor = hitInfo.transform.GetComponent<Renderer>().material.color;
+                foreach (GameObject a in tempList)
+                    a.GetComponent<Renderer>().enabled = false;
+                foreach (Transform child in hitInfo.transform) //changes checkmark to visible
+                    child.GetComponent<Renderer>().enabled = true;
+                print(myColor);
+            }
         }
         if (Input.GetMouseButtonDown(1) && hitInfo.transform.tag == "Block") //HOLD right mouse button
         {
-            StartCoroutine(ClickHold(hitInfo));
+            StartCoroutine(WaitToDestroy(hitInfo));
         }
-        if (Input.GetMouseButtonUp(1)) //if released right mouse button
+        if (Input.GetMouseButtonUp(1)) //if released right mouse button THIS IS KINDA BUGGY D:
         {
-            StopCoroutine(ClickHold(hitInfo));
+            StopCoroutine(WaitToDestroy(hitInfo));
             iTween.ScaleTo(hitInfo.transform.gameObject, new Vector3(1f, 1f, 1f), 1.5f);
         }
-        if (Input.GetMouseButtonDown(2)) //press middle mouse button
+        if (Input.GetMouseButtonDown(2)) //change cube color based on user's color selection 
         {
-            print(hitInfo);
-            //ChangeCubeColor();
+            ChangeCubeColor(myColor, hitInfo);
         }
     }
 
     // creates a block on a grid system
-    void BuildBlock(GameObject block)
+    void BuildBlock(GameObject block, RaycastHit hitInfo)
     {
-        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo)) //if we hit something
+        if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //if we hit another block, build on it!
         {
-            if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //if we hit another block, build on it!
-            {
-                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x/2), 
-                    Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y/2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z/2)); //pretty sure this calc snaps to a grid
-                StartCoroutine(BuildBlockScale(block, spawnPosition));
+            Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2),
+                Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2)); //pretty sure this calc snaps to a grid
+            StartCoroutine(BuildBlockScale(block, spawnPosition));
 
-            }
-            else if (hitInfo.transform.tag != "UI") //if we didn't hit another block
-            { 
-                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
-                StartCoroutine(BuildBlockScale(block, spawnPosition));
-            }
+        }
+        else if (hitInfo.transform.tag != "UI") //if we didn't hit another block
+        {
+            Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
+            StartCoroutine(BuildBlockScale(block, spawnPosition));
         }
         
     }
 
     // destroys the specified block
     // (should) only get called when ClickHold() successfully finishes executing
-    void DestroyBlock(GameObject destroyedBlock)
+    void DestroyBlock(GameObject destroyedBlock, RaycastHit hitInfo)
     {
-        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo))
+        if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //returns the face of the object we hit
         {
-            if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //returns the face of the object we hit
-            {
-                StartCoroutine(DestroyBlockScale(destroyedBlock, hitInfo));
-                
-            }
+            StartCoroutine(DestroyBlockScale(destroyedBlock, hitInfo));
+
         }
     }
 
     // scales the block down and destroys it
+    // boom!!
     IEnumerator DestroyBlockScale(GameObject destroyedBlock, RaycastHit hitInfo)
     {
         //destroys the initial cube and instantiates the fractured one in its place
@@ -136,17 +138,18 @@ public class BuildSystem : MonoBehaviour
     IEnumerator BuildBlockScale(GameObject block, Vector3 spawnPosition)
     {
         GameObject temp = Instantiate(block, spawnPosition, Quaternion.identity);
+        temp.GetComponent<Renderer>().material.SetColor("_BaseColor", myColor);
         iTween.ScaleFrom(temp, new Vector3(.75f, .75f, .75f), 1.5f);
         yield return new WaitForSeconds(1.5f);
     }
 
     // waits for holdToDestroyTime seconds to destroy the specified block
-    IEnumerator ClickHold(RaycastHit hitInfo)
+    IEnumerator WaitToDestroy(RaycastHit hitInfo)
     {
         iTween.ScaleBy(hitInfo.transform.gameObject, new Vector3(.75f, .75f, .75f), holdToDestroyTime);
         yield return new WaitForSeconds(holdToDestroyTime);
 
-        DestroyBlock(fracturedBlockObject);
+        DestroyBlock(fracturedBlockObject, hitInfo);
     }
 
     // scales the block after changing the color
@@ -159,17 +162,12 @@ public class BuildSystem : MonoBehaviour
     }
 
     // changes the cube color to an arbitrary value lmao
-    public void ChangeCubeColor()
+    public void ChangeCubeColor(Color color, RaycastHit hitInfo)
     {
-        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo)) //if we hit something
+        if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //if we hit another block, build on it!
         {
-            if (hitInfo.transform.tag == "Block" && hitInfo.transform.tag != "UI") //if we hit another block, build on it!
-            {
-                hitInfo.transform.GetComponent<Renderer>().material.SetColor("_BaseColor", Color.cyan);
-                StartCoroutine(ColorScale(hitInfo));
-                print("changed color");
-            }
+            hitInfo.transform.GetComponent<Renderer>().material.SetColor("_BaseColor", color);
+            StartCoroutine(ColorScale(hitInfo));
         }
-        
     }
 }
